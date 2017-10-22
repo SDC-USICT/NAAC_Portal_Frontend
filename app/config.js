9d3c52af3d50d50dc14a6146c3649bca706531d1
Surender Kumar <surender7790@gmail.com>
2017-10-22 11:18:34 +0530

    sub apps in formApp folder

--- app/config.js
+++ app/config.js
@@ -1,2 +1,3 @@
-var BACKEND = 'https://tmacyxev.cloud.dropstack.run'
+//var BACKEND = 'https://tmacyxev.cloud.dropstack.run'
+var BACKEND = 'http://localhost:8000'
 var FRONTEND = 'http://localhost:8080'
