7d22f2e5a75b1fb291b9dd2874e813ac669a2277
Surender Kumar <surender7790@gmail.com>
2017-10-23 22:35:29 +0530

    professionalCtrl form submit

--- app/formApp/professional/professionalCtrl.js
+++ app/formApp/professional/professionalCtrl.js
@@ -8,19 +8,67 @@
         .controller('ProfessionalCtrl', Controller);
 
     //  Controller Controller
-    Controller.$inject =   ['$scope'];
-    function Controller($scope) {
+    Controller.$inject =   ['$scope','$http'];
+    function Controller($scope,$http) {
 
         $scope.employee="this is professionalCtrl";
         console.log("from professionalCtrl");
         //  activate logic
-        activate();
+        //console.log($param($scope.naacForm));
 
-        ////////
+        $scope.acadExp;
+        $scope.indExp;
+        $scope.highQual;
+        $scope.qualAdd;
+        $scope.totStu;
+        $scope.phdp;
+        $scope.phds;
+        $scope.phda;
+        $scope.yearProf;
 
-        function activate() {
-          
-            /* - */
+
+
+        console.log($scope.data);
+        console.log($scope.$parent.form_details[$scope.sections[$scope.selected]]);
+
+        $scope.save = function(){
+          $scope.data={
+            employee:$scope.$parent.employee.pk,
+            title: $scope.acadExp,
+            industrial_exp:$scope.indExp,
+            qualification_before:$scope.highQual,
+            qualification_after:$scope.qualAdd,
+            phds:$scope.totStu,
+            pursuing:$scope.phdp,
+            submitted:$scope.phds,
+            awarded:$scope.phda,
+            year:$scope.yearProf
+          }
+          console.log($scope.data);
+          //Submitt form
+
+          var rq = {
+            'kls' : $scope.attributes[$scope.selected].key,
+            'data' :  [$scope.data]
+          }
+          $http.post(BACKEND+'/api/post', JSON.stringify(rq))
+          .then(function (res) {
+            if(res.data.error) {
+              Materialize.toast('Oops! Error', 4000)
+              return;
+            }
+            Materialize.toast('Data Saved Successfully!', 4000)
+            raw = res.data.data;
+
+            $scope.selectedResult =  $scope.results[$scope.attributes[$scope.selected].key].length-1;
+
+
+          });
+
+
+
+
+
         }
     }
 
