eef5f1e42785e93323c25b30526e002a13b2a773
Hemang Kumar <hemangsk@gmail.com>
2017-10-27 02:36:50 +0530

    subjects_taken: Improve layout

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
-//var BACKEND = 'https://tmacyxev.cloud.dropstack.run'
-var BACKEND = 'http://localhost:8000'
+var BACKEND = 'https://tmacyxev.cloud.dropstack.run'
+//var BACKEND = 'http://localhost:8000'
 var FRONTEND = 'http://localhost:8080'
