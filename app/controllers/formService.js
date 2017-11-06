a1ef843d6636887416caaa181c66ad24fb742de1
Surender Kumar <surender7790@gmail.com>
2017-11-06 21:32:19 +0530

    save and delete sidebar

--- app/controllers/formService.js
+++ app/controllers/formService.js
@@ -21,12 +21,14 @@
         function post(element) {
             /* This is a validation factory */
             console.log("From Form service");
-            $http.post(BACKEND+'/api/post', JSON.stringify(element))
+            return $http.post(BACKEND+'/api/post', JSON.stringify(element))
             .then(function (res) {
               if(res.data.error) {
                 Materialize.toast('Oops! Error', 4000)
                 return;
               }
+              //console.log(res.data.data);
+              return res.data.data;
               Materialize.toast('Data Saved Successfully!', 4000)
 
 
