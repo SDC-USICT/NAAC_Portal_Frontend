4fd6f2b5a9eaac9b105b21c6133d8aa74ed8f820
Sameer Kumar <sameer27995@gmail.com>
2017-11-08 19:24:28 +0530

    Frgot password

--- app/controllers/forgot.js
+++ app/controllers/forgot.js
@@ -1,18 +1,24 @@
 angular.module('employee')
 	.controller('ForgotCtrl', ["$scope", "$http" , "$rootScope", "$sessionStorage", "$location",
 		function ($scope, $http, $rootScope, $sessionStorage, $location) {
-
+			if($sessionStorage.loginid == undefined){
+					$location.path('/index');
+			}
+			$scope.id = $sessionStorage.loginid;
 			$scope.submit = function () {
-				mail = {
+				$scope.mail = {
 				'email' : $scope.email,
-				'empid' : $rootScope.loginid
+				'empid' : $scope.id
 			};
-				$http.post(BACKEND+'/api/forgot', JSON.stringify(mail))
+			console.log("Email data");
+			console.log($scope.id);
+				$http.post(BACKEND+'/api/forgot', JSON.stringify($scope.mail))
 				.then(function (res) {
+					console.log("Email data");
 					console.log(res.data);
 					if (res.data.success != undefined) {
 					    console.log("Reidrecting");
-						$location.url('/login');
+						//$location.path('/login');
 						$scope.error = "false";
 					}else{
 						alert('Error during sending email');
