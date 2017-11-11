185411c3b346e80c5b1b377b39e2a8c9a3d59ef3
Sameer Kumar <sameer27995@gmail.com>
2017-11-11 21:41:37 +0530

    Frontend changes

--- app/controllers/forgot.js
+++ app/controllers/forgot.js
@@ -14,8 +14,8 @@ angular.module('employee')
 					if(res.data.error != undefined){
 						Materialize.toast('Please Enter Correct Email ID', 4000);
 					}else if (res.data.success != undefined) {
-					    Materialize.toast('Check Your Email : ' + $scope.email + ' For Password', 4000);
 						$location.path('/login');
+					    Materialize.toast('Check Your Email : ' + $scope.email + ' For Password', 6000);
 					}
 				});
 			}
