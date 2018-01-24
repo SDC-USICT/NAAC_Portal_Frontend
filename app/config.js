dddbd9c76a438e6a9e530157587234103b6220fe
Hemang Kumar <hemangsk@gmail.com>
2018-01-25 04:15:47 +0530

    Update server url

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
-var BACKEND = 'https://bmvltszx.cloud.dropstack.run '
+var BACKEND = 'https://bmvltszx.cloud.dropstack.run'
 //var BACKEND = 'http://localhost:8000'
 var FRONTEND = 'http://localhost:8080'
