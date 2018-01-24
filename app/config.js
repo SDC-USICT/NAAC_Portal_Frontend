fc0b4e4283fd08c6c10d1a3ef52ad7fd6d057ed9
Hemang Kumar <hemangsk@gmail.com>
2018-01-25 04:14:11 +0530

    Update server url

--- app/config.js
+++ app/config.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-var BACKEND = 'https://jmvhzirv.cloud.dropstack.run'
-=======
-var BACKEND = 'https://znwixssy.cloud.dropstack.run'
->>>>>>> 2093f1d... Update backend url
-//var BACKEND = 'http://localhost:8000'
+var BACKEND = 'https://bmvltszx.cloud.dropstack.run '
 var FRONTEND = 'http://localhost:8080'
